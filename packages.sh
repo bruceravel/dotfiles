15cd0487fb97fe1cf92f3eb2681aaa0d6c926eec
Bruce Ravel <bravel@bnl.gov>
2015-03-08 09:29:58 -0400

    more apt-get packages

--- packages.sh
+++ packages.sh
@@ -3,7 +3,7 @@
 packages="gkrellm aptitude texlive-full git emacs python-pip cpanplus
           cpanminus gnuplot-qt thunderbird konqueror konq-plugins
           libncurses-dev gfortran libpng-dev libgif-dev libjpeg8-dev
-          pandoc moodbar highlight vlc"
+          pandoc moodbar highlight vlc valgrind autoconf cifs-utils"
 
 apt-get install $packages
 
