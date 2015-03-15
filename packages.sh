a8eab614263cb5950bae39bc60d9db2e03b3522a
Bruce Ravel <bravel@bnl.gov>
2015-03-15 13:06:16 -0400

    more packages

--- packages.sh
+++ packages.sh
@@ -4,7 +4,9 @@ packages="gkrellm aptitude texlive-full git emacs python-pip cpanplus
           cpanminus gnuplot-qt thunderbird konqueror konq-plugins
           libncurses-dev gfortran libpng-dev libgif-dev libjpeg8-dev
           pandoc moodbar highlight vlc valgrind autoconf cifs-utils
-          gmtp keymon"
+          gmtp keymon perl-doc albatross-gtk-theme bluebird-gtk-theme
+          greybird-gtk-theme human-gtk-theme numix-gtk-theme orion-gtk-theme
+          ttf-mscorefonts-installer inkscape unoconv latex-beamer auctex"
 
 apt-get install $packages
 
